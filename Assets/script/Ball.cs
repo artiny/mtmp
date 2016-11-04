99c952d4cb42d3822abb3ffd9b2351e6504ae3f1
artiny <artiny16@gmail.com>
2016-11-04 20:59:39 +0100

    #3 fent a kupak
    #3 fent a kupak

---
+++ Assets/script/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+
+	void OnCollisionEnter(Collision Col){
+		if (Col.gameObject.tag == "pelouse") {
+			GetComponent<Rigidbody> ().isKinematic = true;
+			transform.position = GameObject.Find ("Depart").GetComponent<Transform> ().position;
+			GetComponent<Rigidbody> ().isKinematic = false;
+		}
+
+		if(Col.gameObject.tag == "win"){
+			Debug.Log ("nyertel");
+		}
+			
+		
+	}
+
+}
