8fafdbff956ccc5093576b7eaeb7c5e366bb390f
artiny <artiny16@gmail.com>
2016-11-04 17:12:35 +0100

    1

---
+++ Assets/script/ControllerBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//pointer
+		Cursor.lockState = CursorLockMode.Locked;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//pointer Esc
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}
---
+++ Assets/script/Direction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Direction : MonoBehaviour {
+	public int SpeedRotation = 50;
+	public int Force = 1000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = GameObject.Find ("Ball").GetComponent<Transform>().position;
+
+		if (GameObject.Find ("Ball").GetComponent<Rigidbody> ().velocity.magnitude < 0.1) {
+			GameObject.Find ("DirectionLine").GetComponent<MeshRenderer> ().enabled = true;
+			transform.Rotate (Vector3.up * Input.GetAxis ("Mouse X") * Time.deltaTime * SpeedRotation);
+			if (Input.GetButtonDown ("Fire1")) {
+				GameObject.Find ("Ball").GetComponent<Rigidbody> ().AddForce (transform.TransformDirection (Vector3.forward*Force));
+			}
+		
+		}
+			
+		if (GameObject.Find ("Ball").GetComponent<Rigidbody> ().velocity.magnitude > 0.1) {
+			GameObject.Find ("DirectionLine").GetComponent<MeshRenderer> ().enabled = false;
+		}
+	}
+}
