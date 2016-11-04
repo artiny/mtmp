e636cc1ff058b66fd4d7fec975c007f5a7e0c619
artiny <artiny16@gmail.com>
2016-11-04 18:07:35 +0100

    #2
    masodik

--- Assets/script/Direction.cs
+++ Assets/script/Direction.cs
@@ -2,29 +2,59 @@
 using System.Collections;
 
 public class Direction : MonoBehaviour {
+	
 	public int SpeedRotation = 50;
-	public int Force = 1000;
+	public int Force = 0;
+	public TextMesh TxtForce;
+
 	// Use this for initialization
 	void Start () {
-	
+		//pointer
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		//pointer Esc
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		
 		transform.position = GameObject.Find ("Ball").GetComponent<Transform>().position;
 
 		if (GameObject.Find ("Ball").GetComponent<Rigidbody> ().velocity.magnitude < 0.1) {
 			GameObject.Find ("DirectionLine").GetComponent<MeshRenderer> ().enabled = true;
+			TxtForce.GetComponent<MeshRenderer> ().enabled = true;
+
 			transform.Rotate (Vector3.up * Input.GetAxis ("Mouse X") * Time.deltaTime * SpeedRotation);
 			if (Input.GetButtonDown ("Fire1")) {
 				GameObject.Find ("Ball").GetComponent<Rigidbody> ().AddForce (transform.TransformDirection (Vector3.forward*Force));
 			}
-		
+			if (Input.GetButton ("Fire2")) 
+			{
+				if (Force < 5010) {
+					Force += 10;
+				} else
+				{
+					Force = 0;
+				}
+			
+				TxtForce.text = "" + Force/50 + "%";
+			}				
+			if (Input.GetMouseButtonDown (2)) 
+			{
+				Force = 0;
+				TxtForce.text = "" + Force/50 + "%";
+			}
+
+
 		}
 			
 		if (GameObject.Find ("Ball").GetComponent<Rigidbody> ().velocity.magnitude > 0.1) {
 			GameObject.Find ("DirectionLine").GetComponent<MeshRenderer> ().enabled = false;
+			TxtForce.GetComponent<MeshRenderer> ().enabled = false;
 		}
 	}
 }
